01179ac571a758cd3663e1bac12690d7aa1c7130
Peter Tynan <pettynan@amazon.com>
2019-03-31 15:33:19 -0700

    problem 1 complete

--- lab-a/starter-code/app.js
+++ lab-a/starter-code/app.js
@@ -10,10 +10,16 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
 
+  var answer1 = [];
+
+  answer1.push(a + b);
+  answer1.push('The sum of ' + a + ' and ' + b + ' is ' + answer1[0] + '.');
+
+  return answer1;
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -97,7 +103,7 @@ function multiplyArray(multArr) { //eslint-disable-line
 // Here is the test for multiplyArray(); uncomment it to run it
 // testMultiplyArray(testArray);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
 // You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
 
